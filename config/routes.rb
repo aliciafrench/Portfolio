423859b0720204f1ae9b75d777e77c706cee68c4
Alicia French <alienor18@yahoo.com>
2014-03-03 22:14:19 -0800

    Added menu and modified style homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FoundationAQv2::Application.routes.draw do
+  resources :posts
+
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
